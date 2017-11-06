9396d83bbead58eb93ece4f70c80556517859dad
awaissikandar <33310387+awaissikandar@users.noreply.github.com>
2017-11-06 09:24:34 -0800

    Create g+.js

---
+++ g+.js
@@ -0,0 +1,5 @@
+<!-- Place this tag in your head or just before your close body tag. -->
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<!-- Place this tag where you want the widget to render. -->
+<div class="g-follow" data-annotation="vertical-bubble" data-height="24" data-href="//plus.google.com/u/0/111818492561788636734" data-rel="author"></div>
